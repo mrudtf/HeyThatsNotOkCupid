165fb7a3c3cc59b6757570e8e94a97c0cbfa6d87
Kiran Helferty <kiranhelferty@gmail.com>
2014-01-02 01:49:33 -0500

    config for my laptop

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,7 @@ HeyThatsNotOkCupid::Application.configure do
 
   # Paperclip with AWS S3
   config.paperclip_defaults = {
+    command_path: "C:\Program Files\ImageMagick-6.8.8-Q16",
     storage: :s3,
     s3_credentials: {
       bucket: "okcdevelopment",
