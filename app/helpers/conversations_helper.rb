19e397aa9b2fe946bef2c12cc44f2025bb4d2b90
Kiran Helferty <kiranhelferty@gmail.com>
2014-01-06 20:41:34 -0500

    conversation styling

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -1,2 +1,6 @@
 module ConversationsHelper
+  def other_user(conversation)
+    user_id = ([conversation.high_user_id, conversation.low_user_id] - [current_user.id]).first
+    User.find(user_id)
+  end
 end
